1c3a9b525748ef0059a1af6a7d2ec0e67facce68
Nicolás Castañeda <nicolascasol@unisabana.edu.co>
2024-03-21 20:09:53 -0500

    .

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,11 @@
-  function toggleAboutMe() {
-    var aboutMeSection = document.querySelector('.main-content');
+function toggleAboutMe() {
+  var aboutMeSection = document.querySelector('.main-content');
 
-    if (aboutMeSection.style.display === 'flex') {
-        aboutMeSection.style.display = 'none';
-    }
-    else {
-        aboutMeSection.style.display = 'flex';
-    }
+  if (aboutMeSection.style.display === 'flex') {
+      aboutMeSection.style.display = 'none';
   }
-  
+  else {
+      aboutMeSection.style.display = 'flex';
+      aboutMeSection.scrollIntoView({ behavior: 'smooth'});
+  }
+}
