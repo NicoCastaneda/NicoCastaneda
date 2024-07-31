f10828ea66b566d959a75ca9be08141858e119f0
Nicolás Castañeda <nicolascasol@unisabana.edu.co>
2024-07-30 22:01:29 -0500

    Actualización total

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+/*!
+=========================================================
+* JohnDoe Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// protfolio filters
+$(window).on("load", function() {
+    var t = $(".portfolio-container");
+    t.isotope({
+        filter: ".new",
+        animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: !1
+        }
+    }), $(".filters a").click(function() {
+        $(".filters .active").removeClass("active"), $(this).addClass("active");
+        var i = $(this).attr("data-filter");
+        return t.isotope({
+            filter: i,
+            animationOptions: {
+                duration: 750,
+                easing: "linear",
+                queue: !1
+            }
+        }), !1
+    });
+});
+
+document.getElementById("downloadCV").addEventListener("click", function() {
+    // Crear un enlace <a> temporal
+    var enlaceDescarga = document.createElement("a");
+    // Configurar el enlace con la ubicación del archivo a descargar
+    enlaceDescarga.href = "CV.txt";
+    // Especificar el nombre del archivo a descargar
+    enlaceDescarga.download = "CV.txt";
+    // Simular clic en el enlace para iniciar la descarga
+    enlaceDescarga.click();
+});
